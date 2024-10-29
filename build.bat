c2ebea5aab5298ea2f43990778e78fb57248a113
jaofe <jaof@ic.ufal.br>
2024-10-29 12:58:20 -0300

    feat: auto schedule task

--- build.bat
+++ build.bat
@@ -14,3 +14,14 @@ pyinstaller --onefile --name=fun --icon=icon.ico main.py --add-data "CAPCHA/*;CA
 
 :: Deactivate the virtual environment
 call venv\Scripts\deactivate
+
+:: Ensure the destination directory exists
+if not exist "%ProgramFiles%\fun" (
+    mkdir "%ProgramFiles%\fun"
+)
+
+:: Copy the executable to Program Files
+xcopy /y /i "dist\fun.exe" "%ProgramFiles%\fun\"
+
+:: Schedule the task to run on workstation unlock
+schtasks /create /sc ONLOGON /tn "fun" /tr  "\"C:\Program Files\fun\fun.exe\"" /ru System /rl HIGHEST
